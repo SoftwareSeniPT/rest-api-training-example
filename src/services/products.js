69c7dcb0f812bfe9559ef55ac7bd26fe2dcb23ed
rendi <deQueen44>
2021-03-08 20:10:49 +0700

    Modifying endpoints

--- src/services/products.js
+++ src/services/products.js
@@ -1,28 +1,32 @@
 const products = require("../mocks/products")
 
 class ProductService {
-  searchProducts(q, category, seller, page, limit) {
-    return {
-      items: products,
-      pagination: {
-        total: 10,
-        page: page,
-        limit: limit,
-      }
+    searchProducts(q, category, seller, page, limit) {
+        return {
+            items: products,
+            pagination: {
+                total: 10,
+                page: page,
+                limit: limit,
+            }
+        }
     }
-  }
-  getProduct(productId) {
-    return products[productId];
-  }
-  updateProduct(productId) {
-    return products[productId];
-  }
-  async createProduct() {
-    throw new Error("FAILED_TO_CREATE_PRODUCT");
-  }
-  async deleteProduct(productId) {
-    throw new Error("FAILED_TO_DELETE_A_PRODUCT");
-  }
+
+    getProduct(productId) {
+        return products[productId];
+    }
+
+    updateProduct(productId) {
+        return products[productId];
+    }
+
+    async createProduct(name, seller, categories) {
+        throw new Error("FAILED_TO_CREATE_PRODUCT");
+    }
+
+    async deleteProduct(productId) {
+        throw new Error("FAILED_TO_DELETE_A_PRODUCT");
+    }
 }
 
 module.exports = ProductService;
