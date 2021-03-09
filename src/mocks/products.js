e2630428bbd99b870d98d20d9ba93442874fc11c
Moch Mufiddin <m.mufiddin@gmail.com>
2021-03-10 06:25:55 +0700

    rest api training

--- src/mocks/products.js
+++ src/mocks/products.js
@@ -1,7 +1,79 @@
-const products = [{
-  name: "Samsung Galaxy S10",
-  seller: "Samsung",
-  categories: ["Phone", "Mobile Device"]
-}];
+const products = [
+  {
+    name: "Samsung Galaxy S10 - 1",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 2",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 3",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 4",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 5",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 6",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 7",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 8",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 9",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 10",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 11",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 12",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 13",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 14",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+  {
+    name: "Samsung Galaxy S10 - 15",
+    seller: "Samsung",
+    categories: ["Phone", "Mobile Device"]
+  },
+];
 
 module.exports = products;
