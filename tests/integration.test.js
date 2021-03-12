682e8f4fd487951b54d03450a35dd7438d5a454b
Hafiz Dharmawan <hafiz@softwareseni.com>
2021-03-12 09:47:25 +0700

    REST API Training

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -8,14 +8,14 @@ describe("products", () => {
     const page = 1;
     const limit = 10;
     const { body, status } = await request(app).get(`/products?page=${page}&limit=${limit}`);
-    expect(status).toBe(httpStatus.CREATED);
+    expect(status).toBe(httpStatus.OK);
     expect(body).toEqual({
-        items: products,
-        pagination: {
-          total: 10,
-          page: page,
-          limit: limit,
-        }
+      items: products,
+      pagination: {
+        total: 10,
+        page: page,
+        limit: limit,
+      }
     });
   });
 
@@ -32,7 +32,7 @@ describe("products", () => {
   });
 
   it('should be returning error when creating a product', async () => {
-    const { body, status } = await request(app).post(`/create-products`).send({
+    const { body, status } = await request(app).post(`/products`).send({
       name: "Samsung S10",
       categories: ["Phones", "Mobile Devices"],
       seller: "Samsung"
@@ -46,9 +46,7 @@ describe("products", () => {
   });
 
   it('should be returning error deleting a product', async () => {
-    const { body, status } = await request(app).delete(`/products`).send({
-      productId: 0
-    });
+    const { body, status } = await request(app).delete(`/products/0`);
     expect(status).toBe(httpStatus.INTERNAL_SERVER_ERROR);
     expect(body).toEqual({
       error: "PRODUCT_ERROR",
@@ -60,25 +58,24 @@ describe("products", () => {
 
 describe("categories", () => {
   it('should be able to bulk delete categories', async () => {
-    const { body, status } = await request(app).post(`/categories/delete`).send({
+    const { body, status } = await request(app).delete(`/categories`).send({
       categoryIds: [1, 2, 3, 4, 5]
     });
-    expect(status).toBe(httpStatus.OK);
+    expect(status).toBe(httpStatus.NO_CONTENT);
     expect(body).toEqual({});
   });
 });
 
 describe("sellers", () => {
   it('should be able to blacklist a seller', async () => {
-    const { body, status } = await request(app).put(`/update-seller-block-status`).send({
-      sellerId: 2,
+    const { body, status } = await request(app).patch(`/seller-block-status/2`).send({
       blacklisted: true
     });
-    expect(status).toBe(httpStatus.NO_CONTENT);
+    expect(status).toBe(httpStatus.OK);
     expect(body).toEqual({});
   });
   it('should be able to search sellers', async () => {
-    const { body, status } = await request(app).post(`/sellers`).send({
+    const { body, status } = await request(app).get(`/sellers`).send({
       seller: "Samsung"
     });
     expect(status).toBe(httpStatus.OK);
