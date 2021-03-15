6b9eaef5ee8a35c317b626fc8fe4de151274e4dc
ervan <ervan@softwareseni.com>
2021-03-15 07:04:42 +0700

    fix: fix the rest api to meet softwareseni best practices

--- src/routes.js
+++ src/routes.js
@@ -11,38 +11,38 @@ const sellerController = new SellerController();
 /*
 * An endpoint to search products based on product name, 
 * product category, and seller name.
-*/ 
+*/
 router.get('/products', productController.searchProducts);
 
 /*
 * An endpoint to get the details of a product.
-*/ 
+*/
 router.get('/products/:productId', productController.getProduct);
 
 /*
 * An endpoint to update the name of the product
 */
-router.put('/products/:productId', productController.updateProduct);
+router.patch('/products/:productId', productController.updateProduct);
 
 /*
 * An endpoint to create a product
 */
-router.post('/create-products', productController.createProduct);
+router.post('/products', productController.createProduct);
 
 /*
 * An endpoint to delete a product
 */
-router.delete('/products', productController.deleteProduct);
+router.delete('/products/:productId', productController.deleteProduct);
 
 /*
 * An endpoint to bulk delete categories
 */
-router.post('/categories/delete', categoriesController.deleteCategories);
+router.delete('/categories', categoriesController.deleteCategories);
 
 /*
 * An endpoint to allow admin to blacklist a seller
 */
-router.put('/update-seller-block-status', sellerController.blockSeller);
+router.patch('/sellers/block', sellerController.blockSeller);
 
 /*
 * An endpoint to search sellers based on seller name and product name
