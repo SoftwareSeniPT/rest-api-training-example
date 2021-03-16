de36b60319b767f9ba29c67ba37e5941c935623f
pandu <pandu@softwareseni.com>
2021-03-16 08:09:11 +0700

    software seni standard code

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,11 +1,11 @@
-const httpStatus = require('http-status');
-const CategoriesService = require('../services/categories')
+const httpStatus = require("http-status");
+const CategoriesService = require("../services/categories");
 
 const categoriesService = new CategoriesService();
 
 class CategoriesController {
   async deleteCategories(req, res) {
-    const { categoryIds } = req.body;
+    const { id } = req.body;
     const result = await categoriesService.deleteCategories(categoryIds);
     res.status(httpStatus.OK).send(result);
   }
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,22 +1,28 @@
-const httpStatus = require('http-status');
-const ProductService = require('../services/products');
+const httpStatus = require("http-status");
+const ProductService = require("../services/products");
 
 const productService = new ProductService();
 
 class ProductController {
   async searchProducts(req, res) {
     const { q, category, seller, page, limit } = req.query;
-    const result = await productService.searchProducts(q, category, seller, parseInt(page), parseInt(limit));
+    const result = await productService.searchProducts(
+      q,
+      category,
+      seller,
+      parseInt(page),
+      parseInt(limit)
+    );
     res.status(httpStatus.CREATED).send(result);
   }
   async getProduct(req, res) {
-    const { productId } = req.params;
-    const result = await productService.getProduct(productId);
+    const { id } = req.params;
+    const result = await productService.getProduct(id);
     res.status(httpStatus.OK).send(result);
   }
   async updateProduct(req, res) {
-    const { productId } = req.params;
-    const result = await productService.updateProduct(productId);
+    const { id } = req.params;
+    const result = await productService.updateProduct(id);
     res.status(httpStatus.OK).send(result);
   }
   async createProduct(req, res) {
@@ -25,29 +31,25 @@ class ProductController {
       const result = await productService.createProduct();
       res.status(httpStatus.CREATED).send(result);
     } catch (error) {
-      res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .send({
-          error: "PRODUCT_ERROR",
-          message: error.message,
-          status: httpStatus.INTERNAL_SERVER_ERROR,
-        });
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+        error: "PRODUCT_ERROR",
+        message: error.message,
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+      });
     }
   }
   async deleteProduct(req, res) {
     try {
       // This service will always be failed
-      const { productId } = req.body;
-      const result = await productService.deleteProduct(productId);
-      res.status(httpStatus.OK).send(result); 
+      const { id } = req.body;
+      const result = await productService.deleteProduct(id);
+      res.status(httpStatus.OK).send(result);
     } catch (error) {
-      res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .send({
-          error: "PRODUCT_ERROR",
-          message: error.message,
-          status: httpStatus.INTERNAL_SERVER_ERROR,
-        });
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+        error: "PRODUCT_ERROR",
+        message: error.message,
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+      });
     }
   }
 }
