4151e23b56c61d96c8b12bdd235b7aa7f2a0bfd0
Salim Romadhon <slmrmdhn@gmail.com>
2021-03-19 13:28:54 +0700

    Done REST API training

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -2,13 +2,14 @@ const request = require("supertest");
 const httpStatus = require('http-status');
 const app = require('../src/app');
 const products = require("../src/mocks/products")
+const apiVersion = "v1"
 
 describe("products", () => {
   it('should be able to search products', async () => {
     const page = 1;
     const limit = 10;
-    const { body, status } = await request(app).get(`/products?page=${page}&limit=${limit}`);
-    expect(status).toBe(httpStatus.CREATED);
+    const { body, status } = await request(app).get(`/${apiVersion}/products?page=${page}&limit=${limit}`);
+    expect(status).toBe(httpStatus.OK);
     expect(body).toEqual({
         items: products,
         pagination: {
@@ -20,19 +21,19 @@ describe("products", () => {
   });
 
   it('should be able to get product details', async () => {
-    const { body, status } = await request(app).get(`/products/0`);
+    const { body, status } = await request(app).get(`/${apiVersion}/products/0`);
     expect(status).toBe(httpStatus.OK);
     expect(body).toEqual(products[0]);
   });
 
   it('should be able to update product name', async () => {
-    const { body, status } = await request(app).put(`/products/0`).send({ name: "Samsung S10" });
+    const { body, status } = await request(app).put(`/${apiVersion}/products/0`).send({ name: "Samsung S10" });
     expect(status).toBe(httpStatus.OK);
     expect(body).toEqual(products[0]);
   });
 
   it('should be returning error when creating a product', async () => {
-    const { body, status } = await request(app).post(`/create-products`).send({
+    const { body, status } = await request(app).post(`/${apiVersion}/products`).send({
       name: "Samsung S10",
       categories: ["Phones", "Mobile Devices"],
       seller: "Samsung"
@@ -46,9 +47,7 @@ describe("products", () => {
   });
 
   it('should be returning error deleting a product', async () => {
-    const { body, status } = await request(app).delete(`/products`).send({
-      productId: 0
-    });
+    const { body, status } = await request(app).delete(`/${apiVersion}/products/0`).send();
     expect(status).toBe(httpStatus.INTERNAL_SERVER_ERROR);
     expect(body).toEqual({
       error: "PRODUCT_ERROR",
@@ -60,25 +59,24 @@ describe("products", () => {
 
 describe("categories", () => {
   it('should be able to bulk delete categories', async () => {
-    const { body, status } = await request(app).post(`/categories/delete`).send({
+    const { body, status } = await request(app).delete(`/${apiVersion}/categories`).send({
       categoryIds: [1, 2, 3, 4, 5]
     });
-    expect(status).toBe(httpStatus.OK);
+    expect(status).toBe(httpStatus.NO_CONTENT);
     expect(body).toEqual({});
   });
 });
 
 describe("sellers", () => {
   it('should be able to blacklist a seller', async () => {
-    const { body, status } = await request(app).put(`/update-seller-block-status`).send({
-      sellerId: 2,
+    const { body, status } = await request(app).put(`/${apiVersion}/sellers/2`).send({
       blacklisted: true
     });
-    expect(status).toBe(httpStatus.NO_CONTENT);
+    expect(status).toBe(httpStatus.OK);
     expect(body).toEqual({});
   });
   it('should be able to search sellers', async () => {
-    const { body, status } = await request(app).post(`/sellers`).send({
+    const { body, status } = await request(app).get(`/${apiVersion}/sellers`).send({
       seller: "Samsung"
     });
     expect(status).toBe(httpStatus.OK);
